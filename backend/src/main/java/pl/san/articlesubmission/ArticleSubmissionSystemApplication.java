efbd504042a0842991e8943bd0b76429e77df34d
Vladyslav Kosolap <vladkosolap12@gmail.com>
2026-05-12 14:18:45 +0200

    chore: initialize full-stack project structure

---
+++ backend/src/main/java/pl/san/articlesubmission/ArticleSubmissionSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.san.articlesubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleSubmissionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleSubmissionSystemApplication.class, args);
+	}
+
+}
